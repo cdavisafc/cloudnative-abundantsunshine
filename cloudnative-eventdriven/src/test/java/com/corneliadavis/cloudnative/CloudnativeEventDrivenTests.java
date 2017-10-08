409bbab4b04b654174be0b822828b7657a973eb1
Cornelia Davis <cornelia@corneliadavis.com>
2017-10-08 08:59:05 -0700

    Fixed test to load full web app context.

--- cloudnative-eventdriven/src/test/java/com/corneliadavis/cloudnative/CloudnativeEventDrivenTests.java
+++ cloudnative-eventdriven/src/test/java/com/corneliadavis/cloudnative/CloudnativeEventDrivenTests.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,7 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = CloudnativeEventDrivenApplication.class)
+@SpringBootTest(classes = CloudnativeEventDrivenApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class CloudnativeEventDrivenTests {
 
@@ -48,51 +50,7 @@ public class CloudnativeEventDrivenTests {
 				.andExpect(status().isOk());
 	}
 
-/*	@BeforeClass
-    public static void initialize() throws Exception {
-	    loadTestData();
-    }
 
-	public void loadTestData() throws Exception {
-
-		user1 = new User("Cornelia", "cdavisafc");
-		user2 = new User("Max", "madmax");
-		user3 = new User( "Glen", "gmaxdavis");
-
-		post1 = new Post(2L, "Max Title", "The body of the post");
-		post2 = new Post(1L, "Cornelia Title", "The body of the post");
-		post3 = new Post(1L, "Cornelia Title2", "The body of the post");
-		post4 = new Post(3L, "Glen Title", "The body of the post");
-
-		connection1 = new Connection(2L, 1L);
-		connection2 = new Connection(1L, 2L);
-		connection3 = new Connection(1L, 3L);
-
-        mockMvc.perform(post("/users").content(mapper.writeValueAsString(user1)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/users").content(mapper.writeValueAsString(user2)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/users").content(mapper.writeValueAsString(user3)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(post("/posts").content(mapper.writeValueAsString(post1)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/posts").content(mapper.writeValueAsString(post2)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/posts").content(mapper.writeValueAsString(post3)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/posts").content(mapper.writeValueAsString(post4)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(post("/connections").content(mapper.writeValueAsString(connection1)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/connections").content(mapper.writeValueAsString(connection2)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        mockMvc.perform(post("/connections").content(mapper.writeValueAsString(connection3)).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-	} */
-/*
 	@Test
 	public void	checkUserCounts() throws Exception {
         mockMvc.perform(get("/users"))
@@ -123,41 +81,6 @@ public class CloudnativeEventDrivenTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$", hasSize(2)));
-	}*/
-
-/*	@Test
-	public void	helloInvalidToken() throws Exception {
-		mockMvc.perform(get("/").cookie(new Cookie("userToken", "1234")))
-				.andExpect(status().isUnauthorized());
 	}
-
-	@Test
-	public void	loginNoName() throws Exception {
-		mockMvc.perform(post("/login"))
-				.andExpect(status().is4xxClientError());
-	}
-
-	@Test
-	public void	loginNamed() throws Exception {
-		mockMvc.perform(post("/login").param("name", "Cornelia"))
-				.andExpect(cookie().exists("userToken"));
-	}
-
-	@Test
-	public void	helloValidToken() throws Exception {
-		assertFalse(CloudnativeHelloworldApplication.validTokens.isEmpty());
-
-		String validToken = CloudnativeHelloworldApplication.validTokens.keySet().iterator().next();
-		String validName = CloudnativeHelloworldApplication.validTokens.get(validToken);
-
-		String query = System.getenv("SEED_INGREDIENT");
-		if (query == null) query = "rice%20flour";
-
-		mockMvc.perform(get("/").cookie(new Cookie("userToken", validToken)))
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString(validName)))
-				.andExpect(content().string(containsString(query)))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.recipes").isEmpty());
-	}*/
 
 }
