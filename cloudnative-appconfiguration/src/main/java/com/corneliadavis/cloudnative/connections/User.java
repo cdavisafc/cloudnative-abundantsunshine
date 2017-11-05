da3750375204cdef71b11e98772fd3f14604164d
Cornelia Davis <cornelia@corneliadavis.com>
2017-11-05 11:32:50 -0500

    Start app config example from end of statelessness.

---
+++ cloudnative-appconfiguration/src/main/java/com/corneliadavis/cloudnative/connections/User.java
@@ -0,0 +1,52 @@
+package com.corneliadavis.cloudnative.connections;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Created by corneliadavis on 9/4/17.
+ */
+@Entity
+@Table(uniqueConstraints=@UniqueConstraint(columnNames="USERNAME"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+
+    protected User() {}
+
+    public User(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
