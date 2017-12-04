eeac2cec254cc55f27814dd24d33625c4424d1da
Cornelia Davis <cornelia@corneliadavis.com>
2017-12-03 16:18:23 -0800

    Move token store out to stateful service (Redis).

--- cloudnative-statelessness/cloudnative-connectionposts/src/main/java/com/corneliadavis/cloudnative/config/CloudnativeApplication.java
+++ cloudnative-statelessness/cloudnative-connectionposts/src/main/java/com/corneliadavis/cloudnative/config/CloudnativeApplication.java
@@ -1,11 +1,16 @@
 package com.corneliadavis.cloudnative.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 //import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.HashMap;
@@ -14,12 +19,23 @@ import java.util.Map;
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
-//@EnableJpaRepositories("com.corneliadavis.cloudnative.*")
 @ComponentScan(basePackages = { "com.corneliadavis.cloudnative.*" })
 @EntityScan("com.corneliadavis.cloudnative.*")
 public class CloudnativeApplication {
 
-	public static Map<String, String> validTokens = new HashMap<String, String>();
+	@Value("${redis.hostname}")
+	private String redisHostName;
+	@Value("${redis.port}")
+	private int redisPort;
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		factory.setHostName(redisHostName);
+		factory.setPort(redisPort);
+		factory.setUsePool(false);
+		return factory;
+	}
 
 	public static void main(String[] args) { SpringApplication.run(CloudnativeApplication.class, args); }
 }
