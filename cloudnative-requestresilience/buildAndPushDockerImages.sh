e4cc526f9ee4ef1c30f2950a3a5aa0a1ce962680
Cornelia Davis <cornelia@corneliadavis.com>
2018-05-05 11:43:16 -0700

    Point to new docker images

--- cloudnative-requestresilience/buildAndPushDockerImages.sh
+++ cloudnative-requestresilience/buildAndPushDockerImages.sh
@@ -1,15 +1,15 @@
 docker build --build-arg \
 jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connectionposts .
+-t cdavisafc/cloudnative-requestresilience-connectionposts .
 #
 docker build --build-arg \
 jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connections .
+-t cdavisafc/cloudnative-requestresilience-connections .
 #
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-posts .
+-t cdavisafc/cloudnative-requestresilience-posts .
 #
-docker push cdavisafc/cloudnative-applifecycle-connectionposts
-docker push cdavisafc/cloudnative-applifecycle-connections
-docker push cdavisafc/cloudnative-applifecycle-posts
+docker push cdavisafc/cloudnative-requestresilience-connectionposts
+docker push cdavisafc/cloudnative-requestresilience-connections
+docker push cdavisafc/cloudnative-requestresilience-posts
