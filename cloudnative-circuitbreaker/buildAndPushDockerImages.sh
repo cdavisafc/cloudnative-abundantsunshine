1163e91df4391d2e2b83d8fbe68e609d2cb21fdd
Cornelia Davis <cornelia@corneliadavis.com>
2018-08-11 11:34:31 -0700

    Posts running with circuit breaker. No fallback behavior.

--- cloudnative-circuitbreaker/buildAndPushDockerImages.sh
+++ cloudnative-circuitbreaker/buildAndPushDockerImages.sh
@@ -1,15 +1,5 @@
 docker build --build-arg \
-jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-requestresilience-connectionposts .
-#
-docker build --build-arg \
-jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-requestresilience-connections .
-#
-docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-requestresilience-posts .
+-t cdavisafc/cloudnative-circuitbreaker-posts:0.0.1 .
 #
-docker push cdavisafc/cloudnative-requestresilience-connectionposts
-docker push cdavisafc/cloudnative-requestresilience-connections
-docker push cdavisafc/cloudnative-requestresilience-posts
+docker push cdavisafc/cloudnative-circuitbreaker-posts:0.0.1
