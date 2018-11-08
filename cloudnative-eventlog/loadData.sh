12f2839f1aceb34e45d138bbdacfa79b40bcf5d7
Cornelia Davis <cornelia@corneliadavis.com>
2018-11-08 10:44:27 +0100

    Fix log message typo. Add sleep in data load script.

--- cloudnative-eventlog/loadData.sh
+++ cloudnative-eventlog/loadData.sh
@@ -7,6 +7,8 @@ curl -X POST -H "Content-Type:application/json" --data '{"follower":"madmax","fo
 curl -X POST -H "Content-Type:application/json" --data '{"follower":"cdavisafc","followed":"madmax"}' localhost:8082/connections
 curl -X POST -H "Content-Type:application/json" --data '{"follower":"cdavisafc","followed":"gmaxdavis"}' localhost:8082/connections
 
+sleep 5
+
 curl -X POST -H "Content-Type:application/json" --data '{"username":"madmax","title":"Max Title","body":"The body of Max first post"}' localhost:8081/posts
 curl -X POST -H "Content-Type:application/json" --data '{"username":"cdavisafc","title":"Cornelia Title","body":"The body of Cornelia first post"}' localhost:8081/posts
 curl -X POST -H "Content-Type:application/json" --data '{"username":"cdavisafc","title":"Cornelia Title2","body":"The body of Cornelia second post"}' localhost:8081/posts
