80c32b80a99b9b83b15cc98a3392ffc542a48782
Cornelia Davis <cornelia@corneliadavis.com>
2018-11-12 19:18:11 -0800

    Consolidate kafka service and deployment. Clean up scripts.

--- cloudnative-eventlog/deployServices.sh
+++ cloudnative-eventlog/deployServices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl apply -f spring-cloud-config-server-deployment-kubernetes.yaml
-kubectl apply -f redis-deployment.yaml
+kubectl apply -f zookeeper-deployment.yaml
+kubectl apply -f kafka-deployment.yaml
 kubectl apply -f mysql-deployment.yaml
-# after creating mysql must create the cookbook database
+# after creating mysql must create the cookbook databases
 
