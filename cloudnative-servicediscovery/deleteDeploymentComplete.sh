2033994ec5c479a7c44a01caaf518d08ab42ea3c
Cornelia Davis <cornelia@corneliadavis.com>
2019-02-17 19:37:25 -0800

    Final chapter 8 edits - mostly naming changes.

--- cloudnative-servicediscovery/deleteDeploymentComplete.sh
+++ cloudnative-servicediscovery/deleteDeploymentComplete.sh
@@ -3,21 +3,15 @@
 
 kubectl delete deploy/posts
 kubectl delete deploy/connections
-kubectl delete deploy/connection-posts
-kubectl delete svc/posts
-kubectl delete svc/connections
-kubectl delete svc/connection-posts
+kubectl delete deploy/connectionsposts
 kubectl delete svc/posts-svc
 kubectl delete svc/connections-svc
-kubectl delete svc/connection-posts-svc
+kubectl delete svc/connectionsposts-svc
 
 if [ "$1" = "all" ]; then
 	kubectl delete deploy/mysql
 	kubectl delete deploy/redis
 	kubectl delete deploy/sccs
-	kubectl delete svc/mysql
-	kubectl delete svc/redis
-	kubectl delete svc/sccs
 	kubectl delete svc/mysql-svc
 	kubectl delete svc/redis-svc
 	kubectl delete svc/sccs-svc
