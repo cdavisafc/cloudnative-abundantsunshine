9db18d19facead0d172d50448d35677d32b4aa5f
Cornelia Davis <cornelia@corneliadavis.com>
2019-02-23 20:35:43 -0800

    Add fallback behavior to circuit breaker.

--- cloudnative-circuitbreaker/buildAndPushDockerImages.sh
+++ cloudnative-circuitbreaker/buildAndPushDockerImages.sh
@@ -1,5 +1,9 @@
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-circuitbreaker-posts:0.0.1 .
+-t cdavisafc/cloudnative-circuitbreaker-posts:0.0.2 .
+docker build --build-arg \
+jar_file=cloudnative-posts/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
+-t cdavisafc/cloudnative-circuitbreaker-connections .
 #
-docker push cdavisafc/cloudnative-circuitbreaker-posts:0.0.1
+docker push cdavisafc/cloudnative-circuitbreaker-posts:0.0.2
+docker push cdavisafc/cloudnative-circuitbreaker-connections
