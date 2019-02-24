df818a4a89256827e44f0a4e3a8c23b608cfc901
Cornelia Davis <cornelia@corneliadavis.com>
2019-02-23 20:09:15 -0800

    Basic circuit breaker fronting Posts service.

--- cloudnative-circuitbreaker/buildAndPushDockerImages.sh
+++ cloudnative-circuitbreaker/buildAndPushDockerImages.sh
@@ -1,9 +1,5 @@
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-circuitbreaker-posts:0.0.2 .
-docker build --build-arg \
-jar_file=cloudnative-posts/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-circuitbreaker-connections .
+-t cdavisafc/cloudnative-circuitbreaker-posts:0.0.1 .
 #
-docker push cdavisafc/cloudnative-circuitbreaker-posts:0.0.2
-docker push cdavisafc/cloudnative-circuitbreaker-connections
+docker push cdavisafc/cloudnative-circuitbreaker-posts:0.0.1
--- cloudnative-circuitbreaker/deployApps.sh
+++ cloudnative-circuitbreaker/deployApps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl apply -f cookbook-deployment-posts.yaml
 kubectl apply -f cookbook-deployment-connections.yaml
-kubectl apply -f cookbook-deployment-connectionposts.yaml
+kubectl apply -f cookbook-deployment-connectionsposts.yaml
