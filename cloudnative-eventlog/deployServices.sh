6db62c6abdf68b5271e6e62b4ab379742e2f5109
Cornelia Davis <cornelia@corneliadavis.com>
2019-03-03 16:49:43 -0800

    event-log deployment to Kubernetes.

--- cloudnative-eventlog/deployServices.sh
+++ cloudnative-eventlog/deployServices.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 kubectl apply -f mysql-deployment.yaml
 # after creating mysql must create the cookbook database
+kubectl apply -f zookeeper-deployment.yaml
+kubectl apply -f kafka-deployment.yaml
 
