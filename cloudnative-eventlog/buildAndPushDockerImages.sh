1da1e6487515372a1c0f1bba05b8368935a097f8
Cornelia Davis <cornelia@corneliadavis.com>
2019-03-03 19:09:54 -0800

    fix typo

--- cloudnative-eventlog/buildAndPushDockerImages.sh
+++ cloudnative-eventlog/buildAndPushDockerImages.sh
@@ -1,13 +1,13 @@
 docker build --build-arg \
-jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.2-SNAPSHOT.jar \
+jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
 -t cdavisafc/cloudnative-eventlog-connectionposts:0.0.2 .
 #
 docker build --build-arg \
-jar_file=cloudnative-connections/target/cloudnative-connections-0.0.2-SNAPSHOT.jar \
+jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
 -t cdavisafc/cloudnative-eventlog-connections:0.0.2 .
 #
 docker build --build-arg \
-jar_file=cloudnative-posts/target/cloudnative-posts-0.0.2-SNAPSHOT.jar \
+jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
 -t cdavisafc/cloudnative-eventlog-posts:0.0.2 .
 #
 docker push cdavisafc/cloudnative-eventlog-connectionposts:0.0.2
