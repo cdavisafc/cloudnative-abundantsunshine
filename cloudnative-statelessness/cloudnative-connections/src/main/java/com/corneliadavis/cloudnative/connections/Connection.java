3304403d6a6df7ddf98014eea636b8dc2a6b8650
Cornelia Davis <cornelia@weave.works>
2020-03-29 20:54:30 -0700

    Update to new java. Update DB ID strategy.

--- cloudnative-statelessness/cloudnative-connections/src/main/java/com/corneliadavis/cloudnative/connections/Connection.java
+++ cloudnative-statelessness/cloudnative-connections/src/main/java/com/corneliadavis/cloudnative/connections/Connection.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Connection {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private Long follower;
     private Long followed;
