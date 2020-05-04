6cdb638b26b6570da225a731e6c9c50e2bfba1be
Cornelia Davis <cornelia@weave.works>
2020-05-03 19:43:52 -0700

    Make sure to tag image with version number (always)

--- cloudnative-applifecycle/buildAndPushDockerImages.sh
+++ cloudnative-applifecycle/buildAndPushDockerImages.sh
@@ -1,15 +1,15 @@
 docker build --build-arg \
 jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connectionposts .
+-t cdavisafc/cloudnative-applifecycle-connectionposts:0.0.2 .
 #
 docker build --build-arg \
 jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connections .
+-t cdavisafc/cloudnative-applifecycle-connections:0.0.2 .
 #
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-posts .
+-t cdavisafc/cloudnative-applifecycle-posts0.0.2 .
 #
-docker push cdavisafc/cloudnative-applifecycle-connectionposts
-docker push cdavisafc/cloudnative-applifecycle-connections
-docker push cdavisafc/cloudnative-applifecycle-posts
+docker push cdavisafc/cloudnative-applifecycle-connectionposts:0.0.2
+docker push cdavisafc/cloudnative-applifecycle-connections:0.0.2
+docker push cdavisafc/cloudnative-applifecycle-posts:0.0.2
